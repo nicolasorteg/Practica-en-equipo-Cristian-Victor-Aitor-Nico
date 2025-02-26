d51a4b4a66c2e9527525b11e1071253520f9ee10
Víctor Marín Escribano <victor.marin4@educa.madrid.org>
2025-02-26 19:14:21 +0100

    añado las clases jugadores,entrenador,persona

---
+++ src/main/kotlin/models/persona.kt
@@ -0,0 +1,15 @@
+package models
+
+import java.time.LocalDate
+
+ abstract class Persona(
+    val id:Long,
+    val nombre:String,
+    val apellidos:String,
+    val fechaNacimiento:LocalDate,
+    val fechaIncorporacion:LocalDate,
+    val salario:Double,
+    val pais:String
+){
+
+}
