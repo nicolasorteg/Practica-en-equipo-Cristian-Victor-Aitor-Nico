c8ac342caeb658518eb16a9af04b73f89f53cf46
Nicolás Ortega Fernández <nicolas.ortega4@educa.madrid.org>
2025-03-04 20:36:36 +0100

    Excepciones implementadas y comentadas

--- src/main/kotlin/exception/PersonasExcepcion.kt
+++ src/main/kotlin/exception/PersonasExcepcion.kt
@@ -1,15 +1,29 @@
 package exception
 
-import models.Persona
-
 /**
  * Clase sellada que se encarga de almacenar los tipos de excepciones implementadas en el programa
  */
-sealed class PersonasExcepcion(message: String) : Exception(message) {
+sealed class PersonasException(message: String) : Exception(message) {
 
-    class PersonasStorageExcepcion(message: String) : PersonasExcepcion(message)
+    /**
+     * Excepción que indica un problema con el almacenamiento de personas.
+     *
+     * @param message Mensaje de error.
+     */
+    class PersonasStorageException(message: String): PersonasException(message)
 
-    class PersonaNotFoundException(id: Int) : PersonasExcepcion("Persona no encontrada con id: $id")
-    class PersonasInvalidoException(message: String) : PersonasExcepcion("Persona no válida: $message")
+    /**
+     * Excepción que indica que no encuentra a la persona.
+     *
+     * @param id Identificador personal de la persona que no se ha podido encontrar.
+     */
+    class PersonaNotFoundException(id: Int): PersonasException("Persona no encontrada con id: $id")
+
+    /**
+     * Excepción que indica que los datos de la persona no son válidos
+     *
+     * @param message Mensaje de error
+     */
+    class PersonasInvalidoException(message: String): PersonasException("Persona no válida: $message")
 
 }
