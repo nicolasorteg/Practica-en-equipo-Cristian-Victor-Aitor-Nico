3856c5b957e4014c0acaf281656d9455fbe145fb
Cristian Ortega Gonzalez <cibercristian2003@gmail.com>
2025-05-12 18:55:36 +0200

    screen

--- src/main/kotlin/practicaenequipocristianvictoraitornico/main.kt
+++ src/main/kotlin/practicaenequipocristianvictoraitornico/main.kt
@@ -5,16 +5,15 @@ import javafx.fxml.FXMLLoader
 import javafx.scene.Scene
 import javafx.stage.Stage
 
-class HelloApplication : Application() {
-    override fun start(stage: Stage) {
-        val fxmlLoader = FXMLLoader(HelloApplication::class.java.getResource("hello-view.fxml"))
-        val scene = Scene(fxmlLoader.load(), 320.0, 240.0)
-        stage.title = "Hello!"
-        stage.scene = scene
-        stage.show()
+import javafx.application.Application
+import javafx.stage.Stage
+
+class Main : Application() {
+    override fun start(primaryStage: Stage) {
+        SplashScreen().start(primaryStage)
     }
 }
 
 fun main() {
-    Application.launch(HelloApplication::class.java)
+    Application.launch(Main::class.java)
 }
